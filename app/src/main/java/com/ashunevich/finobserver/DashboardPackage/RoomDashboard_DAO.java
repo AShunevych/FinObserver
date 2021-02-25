096302d9d1035304e5361d1454cebecd7da10630
AShunevich <48925983+AShunevich@users.noreply.github.com>
2021-02-25 16:57:03 +0200

    - code optimization

--- app/src/main/java/com/ashunevich/finobserver/DashboardPackage/RoomDashboard_DAO.java
+++ app/src/main/java/com/ashunevich/finobserver/DashboardPackage/RoomDashboard_DAO.java
@@ -1,5 +1,7 @@
 package com.ashunevich.finobserver.DashboardPackage;
 
+import com.ashunevich.finobserver.UtilsPackage.FactoryDAO;
+
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
@@ -12,19 +14,10 @@ import androidx.room.Update;
 
 
 @Dao
-public interface RoomDashboard_DAO {
-
-    @Insert (onConflict = OnConflictStrategy.REPLACE)
-    void insert(Dashboard_Account... dashboard_accounts);
-
-    @Delete
-     void delete(Dashboard_Account account);
+public interface RoomDashboard_DAO extends FactoryDAO<Dashboard_Account> {
 
     @Query("DELETE FROM active_accounts")
      void deleteAll();
-
-    @Update
-     void update(Dashboard_Account... dashboard_accounts);
 
     @Query("SELECT * FROM active_accounts" )
     LiveData<List<Dashboard_Account>> getAllAccounts();
--- app/src/main/java/com/ashunevich/finobserver/TransactionsPackage/RoomTransactions_DAO.java
+++ app/src/main/java/com/ashunevich/finobserver/TransactionsPackage/RoomTransactions_DAO.java
@@ -1,6 +1,8 @@
 package com.ashunevich.finobserver.TransactionsPackage;
 
 
+import com.ashunevich.finobserver.UtilsPackage.FactoryDAO;
+
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
@@ -10,10 +12,7 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 @Dao
-public interface RoomTransactions_DAO {
-
-    @Insert (onConflict = OnConflictStrategy.REPLACE)
-    void insert(Transaction_Item...items);
+public interface RoomTransactions_DAO extends FactoryDAO<Transaction_Item> {
 
     @Query ("SELECT * FROM transactions ORDER BY itemID DESC")
     LiveData<List<Transaction_Item>> getAllTransactions();
